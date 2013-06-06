b70a183b40e606f79e9977a9b073b33a94f332af
Michael Noack <michael@noack.com.au>
2013-06-06 14:30:13 +0930

    Improve UI and add clearing of geocode cache

--- app/models/cache.rb
+++ app/models/cache.rb
@@ -1,13 +1,32 @@
 class Cache
+  def self.core_types
+    %w(
+      all_product_types
+      all_countries
+      all_property_types
+      all_vehicle_types
+      all_payment_types
+      all_locations
+      all_passenger_types
+      all_regions
+    )
+  end
+
+  def self.other_types
+    %w(
+      any_exclusive_for
+      allowed_client_type_ids
+      trip_routes
+      properties
+      geocode
+    )
+  end
+
   def self.clear_core
-    caches = %w(all_product_types all_countries all_property_types all_vehicle_types all_payment_types all_locations all_passenger_types all_regions)
-    caches.each { |cache| QuickTravel::Cache.cache_store.delete(cache) }
+    core_types.each { |key| QuickTravel::Cache.cache_store.delete(key) }
   end
 
   def self.clear_other
-    QuickTravel::Cache.cache_store.delete_matched /any_exclusive_for_*/
-    QuickTravel::Cache.cache_store.delete_matched /allowed_client_type_ids_*/
-    QuickTravel::Cache.cache_store.delete_matched /trip_routes_*/
-    QuickTravel::Cache.cache_store.delete_matched /properties_*/
+    other_types.each { |key| QuickTravel::Cache.cache_store.delete_matched /#{key}_*/ }
   end
 end
